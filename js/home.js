b9235d38fda81a031d156ce3acb239872096d4d4
William <s3864077@rmit.edu.vn>
2022-03-31 16:46:54 +0700

    animation cuc ngau

--- js/home.js
+++ js/home.js
@@ -1,40 +1,84 @@
 const filmContainer1 = document.querySelector(".korean");
 const filmContainer2 = document.querySelector(".trending");
+const createElements = (x) => {
+  // swiper-slide
+  var col = document.createElement("div");
+  col.className = "swiper-slide";
 
+  let card = document.createElement("div");
+  card.className = "card";
+  col.appendChild(card);
+
+  // Image
+  let img = document.createElement("img");
+  img.className = "thumbTile__image";
+  img.src = x.thumbnail;
+  card.appendChild(img);
+
+  var cardContent = document.createElement("div");
+  cardContent.className = "card-content";
+  card.appendChild(cardContent);
+
+  // Title
+  let title = document.createElement("h2");
+  title.className = "card-title";
+  title.textContent = x.title;
+  cardContent.appendChild(title);
+  //description
+  let description = document.createElement("p");
+  description.className = "card-body";
+  description.textContent = x.originalTitle;
+  cardContent.appendChild(description);
+  
+  // Button
+  let button = document.createElement("button");
+  button.textContent = "More Detail";
+  button.className = "button";
+  cardContent.appendChild(button);
+
+  return col;
+};
 // let filmClassstart =
 //   '<div class="swiper-slide"><a class="thumbTile" href="#"><img class="thumbTile__image" src="';
 // let filmClassend = '" alt="Suits"></a></div>';
 let filmClassstart =
   '<div class="swiper-slide"><img class="thumbTile__image" src="';
 let filmClassend = '"></div>';
-test = "https://static2.vieon.vn/vieplay-image/thumbnail_v4/2020/12/27/6aiiawke_1920x1080_conangxinhdep_296_168.webp"
+test =
+  "https://static2.vieon.vn/vieplay-image/thumbnail_v4/2020/12/27/6aiiawke_1920x1080_conangxinhdep_296_168.webp";
 fetch("https://dramaholic.herokuapp.com/api/movies")
   .then((respone) => respone.json())
   .then((data) => data._embedded)
   .then((embedded) => embedded.movies)
   .then((movies) => {
     movies.forEach((movie) => {
-      (filmContainer1.innerHTML += filmClassstart + movie.thumbnail + filmClassend)
+      // filmContainer1.innerHTML +=
+      // filmClassstart + movie.thumbnail + filmClassend;
+      const movieL = createElements(movie);
+      filmContainer1.appendChild(movieL);
     });
   });
 
-  fetch("https://dramaholic.herokuapp.com/api/movies?page=1")
+fetch("https://dramaholic.herokuapp.com/api/movies?page=1")
   .then((respone) => respone.json())
   .then((data) => data._embedded)
   .then((embedded) => embedded.movies)
   .then((movies) => {
     movies.forEach((movie) => {
-      (filmContainer2.innerHTML += filmClassstart + movie.thumbnail + filmClassend)
+      // filmContainer2.innerHTML +=
+      // filmClassstart + movie.thumbnail + filmClassend;
+      const movieL = createElements(movie);
+      filmContainer2.appendChild(movieL);
     });
   });
 // End Swipe
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-  spaceBetween: 6,
+  spaceBetween: 5,
   slidesPerView: 2,
   loop: true,
   freeMode: true,
-  loopAdditionalSlides: 6,
+  loopAdditionalSlides: 5,
   speed: 500,
   // Navigation arrows
   navigation: {
@@ -44,8 +88,8 @@ const swiper = new Swiper(".swiper", {
   breakpoints: {
     // when window width is >= 640px
     640: {
-      slidesPerView: 6,
-      slidesPerGroup: 6,
+      slidesPerView: 5,
+      slidesPerGroup: 5,
       freeMode: false,
     },
   },
