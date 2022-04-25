2a2dcc1aea4f297ff1d97dc8211930311feac337
s <s3864077@rmit.edu.vn>
2022-04-25 15:40:12 +0700

    working

--- js/movieList.js
+++ js/movieList.js
@@ -1,20 +1,56 @@
-const movieListElement = document.querySelector(".movie-list");
-const list = [];
+const movieListArray = [];
+
+const list_element = document.getElementById("list");
+const pagination_element = document.getElementById("pagination");
+
+let current_page = 1;
+let rows = 20;
+
+list_element.innerHTML = "";
+fetch("https://dramaholic.herokuapp.com/api/movies")
+  .then((respone) => respone.json())
+  .then((data) => {
+    let page_count = data.totalPages;
+    for (let i = 1; i < page_count; i++) {
+      let btn = PaginationButton(i, data.content);
+      pagination_element.appendChild(btn);
+    }
+    DisplayList(data.content, list_element);
+  });
 function createDivMovie(x) {
   let title = document.createElement("h2");
   title.className = "card-title";
   title.textContent = x.title;
   return title;
 }
-async function getMovieList(movieListArray) {
-  const movieList = await fetch("https://dramaholic.herokuapp.com/api/movies");
-  const response = await movieList.json();
-  const content = await response.content;
-  for (let i = 0; i < content.length; i++) {
-    await movieListArray.push(createDivMovie(content[i]));
-  }
-  for (let i = 0; i < movieListArray.length; i++) {
-    await movieListElement.appendChild(movieListArray[i]);
+function getPage(index, list) {}
+function DisplayList(items, wrapper) {
+  wrapper.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    wrapper.appendChild(createDivMovie(items[i]));
   }
 }
-getMovieList(list);
+function PaginationButton(page, items) {
+  let button = document.createElement("button");
+  button.innerText = page;
+
+  if (current_page == page) button.classList.add("active");
+  button.addEventListener("click", function () {
+    current_page = page;
+    fetch("https://dramaholic.herokuapp.com/api/movies?page=" + current_page)
+      .then((respone) => respone.json())
+      .then(({ content }) => {
+        items = [];
+        for (let i = 0; i < content.length; i++) {
+          items.push(content[i]);
+        }
+        DisplayList(items, list_element);
+      });
+    let current_btn = document.querySelector(".pagenumbers button.active");
+    current_btn.classList.remove("active");
+
+    button.classList.add("active");
+  });
+
+  return button;
+}
