de8a35c0664bd0bf5e84d3b4b18903762b84b0d1
William <s3864077@rmit.edu.vn>
2022-04-29 19:02:19 +0700

    xong category

--- js/category.js
+++ js/category.js
@@ -1,4 +1,8 @@
 const historyContent = document.querySelector(".movie-list-grid");
+const pagination_element = document.getElementById("pagination");
+let current_page = 1;
+let rows = 5;
+
 const createCardHistory = (x) => {
   let card = document.createElement("div");
   card.className = "movie-search-card";
@@ -34,22 +38,69 @@ const createCardHistory = (x) => {
 
   return card;
 };
+const movieList = [];
+
+const category = document.querySelector(".category");
+
+function DisplayList(items, wrapper, rows_per_page, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rows_per_page * page;
+  let end = start + rows_per_page;
+  let paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i];
+    console.log(item)
+
+    historyContent.appendChild(item);
+  }
+}
+
+function SetupPagination(items, wrapper, rows_per_page) {
+  wrapper.innerHTML = "";
+  console.log(items);
+  let page_count = Math.ceil(items.length / rows_per_page);
+  for (let i = 1; i < page_count + 1; i++) {
+    let btn = PaginationButton(i, items);
+    wrapper.appendChild(btn);
+  }
+}
+
+function PaginationButton(page, items) {
+  let button = document.createElement("button");
+  button.innerText = page;
+
+  if (current_page == page) button.classList.add("active");
+
+  button.addEventListener("click", function () {
+    current_page = page;
+    DisplayList(items, historyContent, rows, current_page);
+
+    let current_btn = document.querySelector(".pagenumbers button.active");
+    current_btn.classList.remove("active");
+
+    button.classList.add("active");
+  });
+
+  return button;
+}
+
 async function displayCategory(list, categoryType) {
   const res = await fetch(
-    "https://dramaholic.herokuapp.com/api/movies/search?genre=" + encodeURIComponent(categoryType)
+    "https://dramaholic.herokuapp.com/api/movies/search?genre=" +
+      encodeURIComponent(categoryType)
   );
+  list = [];
   const { content } = await res.json();
   for (let i = 0; i < content.length; i++) {
     await list.push(createCardHistory(content[i]));
   }
-  for (let i = 0; i < list.length; i++) {
-    await historyContent.appendChild(list[i]);
-  }
+  SetupPagination(list, pagination_element, rows);
+  DisplayList(list, historyContent, rows, current_page);
 }
 
-const movieList = [];
-
-const category = document.querySelector(".category");
 // select category
 category.addEventListener("click", function () {
   var options = category.querySelectorAll("option");
@@ -67,3 +118,4 @@ function addActivityItem(option) {
   historyContent.innerHTML = "";
   displayCategory(movieList, option);
 }
+displayCategory(movieList, "Action & Adventure");
