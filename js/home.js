6be481f47209151d4995fa85eef169371ceaf583
William <s3864077@rmit.edu.vn>
2022-05-01 19:17:21 +0700

    blah blah

--- js/home.js
+++ js/home.js
@@ -83,7 +83,7 @@ async function setSwiper() {
   const swiper = new Swiper(".swiper", {
     // Optional parameters
     spaceBetween: 5,
-    slidesPerView: 1,
+    slidesPerView: 2,
     loop: true,
     freeMode: true,
     loopAdditionalSlides: 5,
@@ -96,9 +96,11 @@ async function setSwiper() {
     breakpoints: {
       // when window width is >= 640px
       640: {
+        
         slidesPerView: 5,
         slidesPerGroup: 5,
         freeMode: false,
+
       },
     },
   });
@@ -147,7 +149,7 @@ async function getTrending() {
   // matched or not
   var matched = window.matchMedia(media_query).matches;
   if (matched) {
-    for (let i = 1; i < totalPages; i++) {
+    for (let i = 1; i < movieListTitle.length; i++) {
       await getAllMovie(i);
     }
   }
